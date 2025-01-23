dea8f4e6d802c7a44a1fa884a2d5c9a6ffadfb41
skarl <skarl@LAPTOP-GRO5T35V>
2025-01-22 21:33:08 -0500

    carga inicial

---
+++ src/proyecto1_clase4/Proyecto1_clase4.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package proyecto1_clase4;
+
+import java.util.Scanner;
+public class Proyecto1_clase4 {
+    public static void main(String[] args) {
+        //declarar variables
+        int n1, n2, suma;
+        Scanner lectura = new Scanner(System.in);
+        //entrada de datos
+        System.out.print("Ingresar Numero 1:");
+        n1=lectura.nextInt();
+        System.out.print("Ingresar Numero 2:");
+        n2=lectura.nextInt();
+        //proceso de datos
+        suma=n1+n2;
+        //salida de datos
+        System.out.println("La suma de los numeros es:"+suma);
+    }
+    
+
+}
